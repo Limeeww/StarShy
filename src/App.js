bc2625a739dc0687594d9ab65e8f06b978cfd505
Anastasiia <nastialime569@gmail.com>
2024-06-12 12:33:01 +0200

    Created pw5-6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Account from './components/account';
 import RegisterForm from './components/register-form';
 import LoginForm from './components/login-form';
 import RestorePassword from './components/restore-password';
+import EditProfile from './components/edit-profile';
+import Geolocation from './components/geolocation';
+import Feedback from './components/feedback';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
                     <Route path="/account" element={<Account/>}/>
                     <Route path="/register" element={<RegisterForm/>}/>
                     <Route path="restore-pass" element={<RestorePassword/>}/>
+                    <Route path="/edit-profile" element={<EditProfile />} />
+                    <Route path="/geolocation" element={<Geolocation />} />
+                    <Route path="/feedback" element={<Feedback />} />
                 </Routes>
             </BrowserRouter>
         </div>
